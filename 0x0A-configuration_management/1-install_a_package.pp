f3c4c6a4cecc7a83e19445e213dc02155eba76a6
millyanne93 <nmillyanne20@gmail.com>
2024-01-20 04:41:13 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
 #!/usr/bin/pup
 # Using Puppet, install flask from pip3
+package {'python3-pip':
+  ensure   => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require => Package['python3-pip'],
+}
+package {'werkzeug':
+  ensure  => '2.1.1',
+  provider => 'pip',
+  require => Package['python3-pip'],
 }
