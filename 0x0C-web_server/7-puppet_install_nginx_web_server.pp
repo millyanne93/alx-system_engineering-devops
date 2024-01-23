ea049101726bb75b7e9c3f5c80bd1aeb32c6a78d
millyanne93 <nmillyanne20@gmail.com>
2024-01-23 05:52:52 -0800

    7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# script to install package using puppet
+
+package {'nginx':
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path => '/etc/nginx/sites-enabled/default',
+  after => 'listen 80 default_server;',
+  line => 'rewrite ^/redirect_me https://www.github.com/millyanne93 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello world',
+}
+
+service { 'nginx':
+  ensure => running,
+  require => Package['nginx'],
+}
