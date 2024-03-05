8232637397312ca037551984f3a3c4b4f478d564
millyanne93 <nmillyanne20@gmail.com>
2024-03-05 19:06:58 +0000

    fix syntax

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,10 @@
 #Fixes bad ""pphp" extensions to "php" in "wp-settings.php".
 
-exec { 'fix-wordpress':
-  command     => 'sed -i s/pphp/php/g /var/www/html/wp-settings.php',
-  path        => '/usr/local/bin/:/bin/'
+$target_file = '/var/www/html/wp-settings.php'
+
+#Fix WordPress configuration by replacing line containing "phpp" with "php"
+
+exec { 'fix_wordpress_config':
+  command => "sed -i 's/phpp/php/g' ${target_file}",
+  path    => ['/bin','/usr/bin']
 }
