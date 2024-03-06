ea50bc7de2ae57a0d3e2c3c4930037b61b854a2b
millyanne93 <nmillyanne20@gmail.com>
2024-03-06 01:50:47 -0800

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 
 ''' This module contains the function number_of_subscribers '''
-import json
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    ''' Gets the number of subscribers of a subreddit '''
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
     try:
-        response = requests.get("https://www.reddit.com/r/{}/about.json"
-                                .format(subreddit),
-                                headers={"User-Agent": "GetSubscriberCount"},
-                                allow_redirects=False)
-        data = response.json().get('data')
-        if data:
-            return data['subscribers']
-        else:
-            return 0
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
