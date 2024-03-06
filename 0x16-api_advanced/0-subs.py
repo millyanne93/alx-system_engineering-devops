61a9bfcf5ac63c77bc1a503eab509706c575c093
millyanne93 <nmillyanne20@gmail.com>
2024-03-06 01:47:23 -0800

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     ''' Gets the number of subscribers of a subreddit '''
-    response = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "GetSubscriberCount"},
-                            allow_redirects=False)
-
-    data = response.json().get('data')
-    if not data:
+    try:
+        response = requests.get("https://www.reddit.com/r/{}/about.json"
+                                .format(subreddit),
+                                headers={"User-Agent": "GetSubscriberCount"},
+                                allow_redirects=False)
+        data = response.json().get('data')
+        if data:
+            return data['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return 0
-    else:
-        return data['subscribers']
